9339198269eb54f48a2f5d672082303c23d14e30
LiKang <kangli9233@gmail.com>
2021-11-18 23:09:56 -0500

    update

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -11,30 +11,7 @@ export default class Cards extends Component {
           </a>
           <p className="card-text">reactjs</p>
         </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        
       </div>
     )
   }
